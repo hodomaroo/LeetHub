a99744cc07534edac0d686be3b139f126cdb34a0
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-03-20 20:43:50 +0900

    Time: 974 ms (70.90%), Space: 26.9 MB (43.16%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -4,6 +4,7 @@ MOD = 1000000007
 
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
+
         #Two - Pointer
         nums.sort()
         left, right = 0, len(nums) - 1
@@ -11,7 +12,7 @@ class Solution:
 
         while left <= right:
             if nums[left] + nums[right] <= target:
-                ans = (ans + 2 ** (right - left)) % MOD
+                ans = (ans + pow(2,right - left,MOD)) % MOD
                 left += 1
             else: right -= 1
         return ans
