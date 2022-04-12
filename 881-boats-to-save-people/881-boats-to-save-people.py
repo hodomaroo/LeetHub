aaff181ad001c3b4820d18548a855eac5a5b6081
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-04-12 21:26:55 +0900

    Time: 640 ms (46.04%), Space: 21.3 MB (5.84%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,30 @@
+from typing import List
+from bisect import bisect_right #탈수있는 가장 무거운 사람
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        # 가장 무거운 사람부터 / 매칭되는 가장 무거운 사람과 함께 가기
+        people.sort()
+        isValid = [True] * len(people)
+        remain = len(people)
+        count = 0
+
+        limitIndex = len(people)
+
+        for i in range(len(people)):
+            if not isValid[i]: continue
+            
+            target = bisect_right(people,limit - people[i] , lo=i + 1, hi=limitIndex) - 1
+            print(i,target)
+            if target > i: 
+                isValid[target] = False
+
+            limitIndex = min(limitIndex, target)
+            count += 1
+
+
+        return count
+print(bisect_right([1,2,3,4,5],5,hi=4))
+
+
+
+
