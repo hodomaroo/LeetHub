a7e33b564fd1e94be2419d99bb05c3a7fd41fc47
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-04-12 21:27:56 +0900

    Time: 770 ms (23.13%), Space: 20.9 MB (75.89%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -5,6 +5,7 @@ class Solution:
         # 가장 무거운 사람부터 / 매칭되는 가장 무거운 사람과 함께 가기
         people.sort()
         isValid = [True] * len(people)
+        
         remain = len(people)
         count = 0
 
@@ -19,9 +20,7 @@ class Solution:
             limitIndex = target
             count += 1
 
-
         return count
-print(bisect_right([1,2,3,4,5],5,hi=4))
 
 
 
