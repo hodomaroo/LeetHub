f8c1889b8c11887420fdf7bb62697383220c1196
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-06-23 19:38:46 +0900

    Time: 818 ms (70.25%), Space: 20.3 MB (6.86%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -0,0 +1,18 @@
+from typing import List
+from heapq import heappop,heappush
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        courses.sort(key = lambda x : x[1])
+        heap = []
+        total = 0
+        for dur, li in courses:
+            #print(heap,total,dur,li)
+            if total + dur > li:
+                if heap and abs(heap[0][0]) > dur and total - abs(heap[0][0]) + dur <= li:
+                    total += dur - (abs(heap[0][0]) if heap else 0)
+                    heappop(heap)
+                    heappush(heap,(-dur, li))
+            else:
+                total += dur
+                heappush(heap,(-dur, li))
+        return len(heap)
