b31e24db7e09993efc0833eab9dcb27c93ea9e9d
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-07-03 16:41:48 +0900

    Time: 534 ms (5.03%), Space: 13.9 MB (77.55%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] == nums[j]: continue
-                print(nums[i] < nums[j],i,j)
+                #print(nums[i] < nums[j],i,j)
                 dp[i][nums[i] < nums[j]] = max(dp[i][nums[i] < nums[j]], dp[j][1 - (nums[i] < nums[j])] + 1)
                 maxLen = max(maxLen, dp[i][nums[i] < nums[j]])
         return maxLen
