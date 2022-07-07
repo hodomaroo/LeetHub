3789cee83b4c70a0e65a94ca496d8cba097b6c83
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-07-08 04:26:06 +0900

    Time: 167 ms (20.94%), Space: 21.5 MB (31.67%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def __init__(self):
         self.ans = -float("inf")
@@ -13,14 +14,15 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.ans = -float("inf")
         def dfs(node : TreeNode) -> int:
-            l = dfs(node.left) if node.left else 0
-            r = dfs(node.right) if node.right else 0
+            l,lMax = dfs(node.left) if node.left else [0,-float("inf")]
+            r,rMax = dfs(node.right) if node.right else [0,-float("inf")]
 
             curVal = node.val + max(0, l, r)
             self.ans = max(self.ans, node.val + l + r, curVal)
-            return curVal
-        
-        dfs(root)
-        return self.ans
+            return [curVal, max(curVal, node.val + l + r, lMax, rMax)]
+
+        return dfs(root)[1]
+
+
 
 
