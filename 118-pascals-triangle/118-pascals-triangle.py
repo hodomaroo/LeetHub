69cf850d88d99f86c5e72cefd2d9d3ef593c31c8
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-07-19 09:44:43 +0900

    Time: 28 ms (97.08%), Space: 13.9 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,14 +1,13 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        arr = []
+        arr = [[0] * i for i in range(1, numRows + 1)]
+        
         for i in range(numRows):
+            arr[i][0] = arr[i][-1] = 1
             
-            arr.append([1])
             for j in range(1,i):                
-                arr[i].append(arr[i-1][j] + arr[i-1][j-1])
-            
-            if i:
-                arr[i].append(1)
+                arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
+
         return arr
             
                 
