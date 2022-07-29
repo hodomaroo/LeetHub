c0a1f4ae8a05342cd026de78fee9789d660aa2d3
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-07-29 23:25:19 +0900

    Time: 33 ms (91.85%), Space: 13.8 MB (65.75%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [[0,0] for _ in range(len(nums) + 1)]
+        dp = [[0,0] for _ in range(len(nums) + 2)]
         
         #dp[i][0] -> not select
         #dp[i][1] -> select
-        for i in range(1, len(nums) + 1):
-            dp[i][0] = max(dp[i-1])
-            dp[i][1] = dp[i-1][0] + nums[i - 1]
-        return max(dp[-1])
+        for i in range(2, len(nums) + 2):
+            dp[i][0] = max(dp[i-2][0] + nums[i-2], dp[i-1][0])
+            
+        return dp[-1][0]
