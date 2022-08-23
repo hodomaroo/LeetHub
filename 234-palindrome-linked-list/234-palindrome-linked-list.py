fcac9a44069f54e257e646626afa7d453143f1ed
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-08-23 16:59:30 +0900

    Time: 839 ms (91.07%), Space: 38.9 MB (89.07%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,13 +5,44 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return True
         
-        values = []
-        while head:
-            values.append(head.val)
+        length = head != None
+        
+        _prev = head
+        while _prev.next:
+            _prev = _prev.next
+            length += 1
+        
+        if length <= 1:
+            return True
+        
+        _prev = head
+        for i in range(length // 2 + length % 2):
+            if i == (length // 2 + length % 2 - 1):
+                sv = _prev.next
+                _prev.next = None
+                _prev = sv
+            else:
+                _prev = _prev.next
+        
+        _next = _prev.next #원래 다음 노드가 가르키던 곳 /
+        _prev.next = None
+        
+        while _next:
+            _nnext = _next.next #다다음 노드! -> 세이브 해놓을 필요 있음
+            _next.next = _prev #다음 노드의 이전 노드(현재 노드)
+            _prev = _next 
+            _next = _nnext #다음노드를 _nnext로 변경
+        
+        while head and _prev:
+            if head.val != _prev.val:
+                return False
             head = head.next
-            
-        for i in range(len(values) // 2):
-            if values[i] != values[-1 - i]: return False
+            _prev = _prev.next
         return True
+                
         
+        
+                  
