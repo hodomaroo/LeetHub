aa9c7c45a02243b4d1a2ba17a86f651521014b4a
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-08-26 16:51:01 +0900

    Time: 555 ms (5.33%), Space: 39.2 MB (96.00%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    ArrayList<Integer> array;
+    boolean[] used;
+    int count = 0;
+    int tarV = 0;
+    
+    public boolean reorderedPowerOf2(int n) {
+        array = new ArrayList<Integer>();
+        
+        while(n > 0){
+            array.add(n % 10);
+            n /= 10;
+        }
+        used = new boolean[array.size()] ;
+        System.out.println(array.toString());
+        
+        dfsFunction(0, 0);
+        return count == 1;
+    };    
+    
+    public void dfsFunction(int v, int depth){
+        if(depth == array.size()){
+            int cnt = 0;
+            for(int i = 0 ; i < 30 ; i++){
+                cnt += (v >> i) & 1;
+            }
+            
+            if (cnt == 1 && v != tarV){
+                tarV = v;
+            
+                count += 1;
+            }
+            return;
+        }
+        
+        
+        for(int i = 0 ; i < array.size() ; i ++){
+            if(used[i] || (depth == 0 && array.get(i) == 0))    continue;
+            used[i] = true;
+            dfsFunction(v * 10 + array.get(i), depth + 1);
+            used[i] = false;
+        }
+    }
+
+}
