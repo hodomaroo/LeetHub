d7d5a555201183a2b0f5b9f151b31230c82c4bbf
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-08-28 22:03:21 +0900

    Time: 10 ms (61.20%), Space: 47.4 MB (79.60%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -8,30 +8,21 @@ class Solution {
     
        return mat;   
     }
-    
-    
+
     public void sortPoint(int[][] matrix, int sx, int sy){
         ArrayList<Integer> diagonalLine = new ArrayList<Integer>();
         
-        int x = sx, y = sy;
-        
         for(int i = 0 ; i < matrix[0].length ; i++){
-            if(x >= matrix.length || y >= matrix[0].length)
+            if(sx + i >= matrix.length || sy + i >= matrix[0].length)
                 break;
-            diagonalLine.add(matrix[x][y]);
-            x += 1;
-            y += 1;
+            diagonalLine.add(matrix[sx + i][sy + i]);
         }   
         Collections.sort(diagonalLine);
-        
-        x = sx;
-        y = sy;
-        
+    
         for(int i = 0 ; i < diagonalLine.size() ; i++){
-            if(x >= matrix.length || y >= matrix[0].length)   break;
-            matrix[x][y] = diagonalLine.get(i);
-            x += 1;
-            y += 1; 
+            if(sx + i >= matrix.length || sy + i >= matrix[0].length) 
+                break;
+            matrix[sx + i][sy + i] = diagonalLine.get(i);
         }   
     }
 }
