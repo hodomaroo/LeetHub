db8868087abbf84358a3b7faf7c286ac8b6cba55
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-09 15:18:10 +0900

    Time: 4837 ms (8.61%), Space: 67.2 MB (63.30%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort(key = lambda x : (-x[0], x[1]))
+        
+        maxi = float("-inf")
+        count = 0
+        
+        for att, dep in properties:
+            if maxi > dep:
+                count += 1
+            maxi = max(dep,maxi)
+            
+        return count
+            
+        
+        
