88833937f0d036b298201b9b88cc679e76de86d4
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-18 16:44:46 +0900

    Time: 169 ms (73.95%), Space: 16.7 MB (7.10%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -8,5 +8,4 @@ class Solution:
             rr[i][0] = max(0 if i == 0 else rr[i-1][0], height[i])
             rr[-i-1][1] = max(0 if i == 0 else rr[-i][1], height[-i - 1])
             
-        
         return sum(max(0,min(rr[i]) - height[i]) for i in range(len(height)))
