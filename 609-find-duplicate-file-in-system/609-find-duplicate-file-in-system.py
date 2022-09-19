ed17b2abba558a8bd44fb00732f80db21b5f2378
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-19 16:04:11 +0900

    Time: 302 ms (5.93%), Space: 24.2 MB (11.36%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        #pathString 분할 (" ")
+        #-> 0 : 경로정보 / ELSE : 파일
+        
+        fileGroup = defaultdict(list)
+        
+        def getInformationOfFile(info : str) -> list[str]:
+            info = info.split(".")
+            fileName = info[0]
+            info = info[1].split("(")
+            fileExpe = info[0]
+            fileContents = info[1][:-1]
+            print(fileName, fileExpe, fileContents)
+            return [fileName, fileExpe, fileContents]
+                
+        #print(getInformationOfFile("1.txt(abcd)"))
+        
+        for path in paths:
+            route, *files = path.split()
+            
+            for file in files:
+                name,expe, contents = getInformationOfFile(file)
+                fileGroup[contents].append(route + "/" + name + "." + expe)
+        
+        return [fileGroup[contents] for contents in fileGroup if len(fileGroup[contents]) > 1]
+            
