46a1ccd22f95c6e7ec99cfd8defa05d2c230a695
hodomaroo <61859590+hodomaroo@users.noreply.github.com>
2022-09-23 13:36:57 +0900

    Time: 4915 ms (11.02%), Space: 13.9 MB (62.20%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -3,6 +3,6 @@ class Solution:
         MOD = pow(10,9) + 7
         cur = 0        
         for i in range(1, n + 1):        
-            cur = ((cur << (int(log2(i)) + 1)) + i) % MOD
+            cur = (cur << (int(log2(i)) + 1)) % MOD + i
             
-        return cur     
+        return cur % MOD
