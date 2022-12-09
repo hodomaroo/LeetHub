ebb88b93ba67988c0c0bd93b690570a600cab129
김두호 <61859590+hodomaroo@users.noreply.github.com>
2022-12-09 23:35:02 +0900

    Time: 54 ms (68.53%), Space: 14.1 MB (9.90%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,11 @@
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node : TreeNode):
+            if not node.left and not node.right:
+                return [node.val]
+            
+            return (dfs(node.left) if node.left else []) + (dfs(node.right) if node.right else [])
+        
+        print(dfs(root1))
+        print(dfs(root2))
+        return dfs(root1) == dfs(root2)
