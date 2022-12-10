d4ae38a1b15d8f1b301920d65afb5fc837646802
김두호 <61859590+hodomaroo@users.noreply.github.com>
2022-12-10 21:47:16 +0900

    Time: 409 ms (82.67%), Space: 80.3 MB (9.38%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        MOD,_sum = 10**9 + 7, dict()
+        def dfs(node : TreeNode) -> int:
+            _sum[node] = node.val + (dfs(node.left) if node.left else 0) + (dfs(node.right) if node.right else 0)
+            print(_sum[node])
+            return _sum[node]
+        dfs(root)
+        
+        print(_sum.values())
+        target = min(_sum.values(), key = lambda x : abs(_sum[root] // 2 - x))
+        print(target, _sum[root] - target)
+        return target * (_sum[root] - target) % MOD
+        
+        
+        
+        
+        
+            
