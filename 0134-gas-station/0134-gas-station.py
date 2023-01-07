35a6981402989f74ea0ff529235eac1671acda14
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-01-08 03:23:34 +0900

    Time: 638 ms (99.21%), Space: 19.2 MB (49.17%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,13 +5,18 @@ class Solution:
         if sum(gas) < sum(cost):
             return -1
         
+        loop = False
         for i in range(len(gas)):
+            if loop and idx == i:
+                return idx
             
             accum += gas[i] - cost[i]
             
             while accum < 0:
                 idx = len(gas) - 1 if not idx else idx - 1
                 accum += gas[idx] - cost[idx]
+                
+            loop = True
                 
         print(idx)
         return idx
