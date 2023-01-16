95194b28e84eaa6bf18ca456f7e959843ce38771
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-01-17 03:30:44 +0900

    Time: 71 ms (99.36%), Space: 17.3 MB (52.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,18 +3,13 @@ class Solution:
         def function(a):
             return (a[0] >= b[0] and a[1] < b[1]) or (a[0] <= b[0] and a[1] >= b[1]) or (a[0] >= b[0] and a[0] <= b[1]) or (a[1] >= b[0] and a[1] <= b[1])
         
-        # if not intervals:
-        #     return [b]
-        
         inter = list(filter(function, intervals))
-        
         
         if not inter:
             node = bisect_left(intervals, b[0], key = lambda x : x[1])
             return intervals[:node] + [b] + intervals[node:]
-        left,right = intervals.index(inter[0]), intervals.index(inter[-1])
         
-        intervals[left:right + 1] = [[min(inter[0][0], b[0]), max(b[1], inter[-1][1])]]
+        intervals[intervals.index(inter[0]):intervals.index(inter[-1]) + 1] = [[min(inter[0][0], b[0]), max(b[1], inter[-1][1])]]
         return intervals
         
         
