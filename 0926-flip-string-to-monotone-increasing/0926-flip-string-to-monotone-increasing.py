89808eca933e77462a9b31d2ed5c4a25e9e71ad6
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-01-17 19:44:12 +0900

    Time: 296 ms (66.03%), Space: 19 MB (24.68%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,7 +1,8 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         dp,leng = [0] + list(accumulate(list(map(int,s)))),len(s)
-        return min(leng - i - dp[-1] + 2 * dp[i] for i in range(len(s) + 1))
+        end = dp[-1]
+        return min(leng - i - end + 2 * dp[i] for i in range(len(s) + 1))
             
 
             
