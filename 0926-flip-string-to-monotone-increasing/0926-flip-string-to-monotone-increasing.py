9c80b73a7fbe7e1e81dd1fcc50ce67bc465d0295
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-01-17 19:16:39 +0900

    Time: 363 ms (58.33%), Space: 19.5 MB (17.63%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,15 +1,6 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         dp = [0] + list(accumulate(list(map(int,s))))
-        
-        #i부터 1로 만든다
-        
-#         print(dp)
-        
-#         for i in range(len(s) + 1):
-#             #i번째까지 1로 채운다면, 아래 개수만큼 필요
-#             print(len(s) - i - (dp[-1] - dp[i]),dp[i])
-            
         return min(len(s) - i - (dp[-1] - dp[i]) + dp[i] for i in range(len(s) + 1))
             
 
