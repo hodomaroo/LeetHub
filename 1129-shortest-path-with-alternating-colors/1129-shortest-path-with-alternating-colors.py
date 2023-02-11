aedc63f39be7f6ce59d03355be574daa8122f8d3
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-02-11 12:48:10 +0900

    Time: 90 ms (70.67%), Space: 14.1 MB (73.17%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,16 +1,13 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
         dp = [[float("inf"),float("inf")] for _ in range(n)]
-        dp[0] = [0,0]
         q = deque([(0,False), (0,True)]) #node / idx
-        
         graph = [[[] for _ in range(n)] for _ in range(2)]
         
-        for a,b in redEdges:
-            graph[0][a].append(b)
+        dp[0] = [0,0]
         
-        for a,b in blueEdges:
-            graph[1][a].append(b)
+        for a,b in redEdges: graph[0][a].append(b)
+        for a,b in blueEdges: graph[1][a].append(b)
             
         while q:
             node,boolean = q.popleft()
