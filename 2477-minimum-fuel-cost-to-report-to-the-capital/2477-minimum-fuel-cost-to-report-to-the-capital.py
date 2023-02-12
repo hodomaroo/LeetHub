9e708741c2a2fa7e6a9d72d1f4f1a5901ce5f70f
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-02-12 10:51:20 +0900

    Time: 1800 ms (99.38%), Space: 154.5 MB (77.57%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        graph = [[] for _ in range(len(roads) + 1)]
+        
+        for a,b in roads:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        def dfs(par : int, node : int) -> List[int]: #Number of Child / CurrentTotalFuel
+            count, fuel = 1,0
+            
+            for child in graph[node]:
+                if child == par: continue
+                    
+                c,f = dfs(node, child)
+                count += c
+                fuel += f + ceil(c / seats) #연료는 그냥 더하면 됨
+            return [count, fuel] 
+                
+        return dfs(-1, 0)[1]
+            
+            
+            
+            
