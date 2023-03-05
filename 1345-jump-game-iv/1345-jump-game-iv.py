e06cc1a3bc41f741184194623345e4dd31df5720
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-03-05 14:01:17 +0900

    Time: 758 ms (82.93%), Space: 27.3 MB (97.07%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,29 +1,31 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
+        MAX = 100000
         queue = deque([0])
         valNodeMap = defaultdict(list)
         
         for i in range(len(arr)):
             valNodeMap[arr[i]].append(i)
             
-        dp = [float("inf")] * len(arr)
+        dp = [MAX] * len(arr)
         dp[0] = 0
         
         while queue:
             node = queue.popleft()
             
-            if node and dp[node - 1] == float("inf"):
+            if node and dp[node - 1] == MAX:
                 dp[node - 1] = dp[node] + 1
                 queue.append(node - 1)
             
-            if node < len(arr) - 1 and dp[node + 1] == float("inf"):
+            if node < len(arr) - 1 and dp[node + 1] == MAX:
                 dp[node + 1] = dp[node] + 1
                 queue.append(node + 1)
                 
             for v in valNodeMap[arr[node]]:
-                if dp[v] != float("inf"): continue
+                if dp[v] != MAX: continue
                 dp[v] = dp[node] + 1
                 queue.append(v)
+                
             valNodeMap[arr[node]] = []
         return dp[-1]
                     
