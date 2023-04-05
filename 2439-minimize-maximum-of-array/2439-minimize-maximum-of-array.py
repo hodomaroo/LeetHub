c95a859d24d544f0c20fb3f0bdefa8df6c4d0d34
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-04-05 12:15:45 +0900

    Time: 849 ms (49.33%), Space: 28.5 MB (7.17%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -4,13 +4,11 @@ class Solution:
         
         for i in range(len(nums) - 1, 0, -1):
             _avg = (_sum - 1) // (i + 1)  + 1#평균
-            print(_avg)
             diff = nums[i] - _avg
             if diff > 0:
                 nums[i - 1] += diff
                 nums[i] -= diff
             
             _sum -= nums[i]
-        
-        print(nums)
+    
         return max(nums)
