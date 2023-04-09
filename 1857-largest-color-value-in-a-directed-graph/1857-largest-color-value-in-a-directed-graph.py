cc35715d3bfc355537dab5606cc7800ad5bced82
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-04-09 14:09:50 +0900

    Time: 3671 ms (8.93%), Space: 186.6 MB (21.43%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -5,19 +5,14 @@ class Solution:
         values = [[MIN] * 26 for _ in range(len(colors))]
         graph = [[] for _ in range(len(colors))]
         INVALID = [MIN] * 26
-        cycle_detected = False
         
         
         for a,b in edges:
             graph[a].append(b)
         
         def dfs(node : int, group : int) -> list[int]:
-            nonlocal cycle_detected
             
-            if visit[node]:
-                if visit[node] == group and visit_2[node]:
-                    cycle_detected = True
-                    
+            if visit[node]:                
                 return INVALID if (group == visit[node] and visit_2[node]) else values[node]
             
             visit_2[node] = True
@@ -25,22 +20,26 @@ class Solution:
             visit[node] = group
             
             for next_node in graph[node]:            
-                for i,v in enumerate(dfs(next_node, group)):
+                next_values = dfs(next_node, group)
+                if next_values == INVALID:  return INVALID
+                
+                for i,v in enumerate(next_values):
                     values[node][i] = max(values[node][i], v)
             
             values[node][ord(colors[node]) - ord('a')] += 1
             
             visit_2[node] = False
             
-            return INVALID if max(values[node]) < 0 else values[node]
+            return values[node]
         
         _ans = MIN
         for i in range(len(colors)):
             if visit[i]: continue
-            _ans = max(_ans, max(dfs(i,i + 1)))
-            if cycle_detected:  return -1
+            result = dfs(i,i + 1)
+            if result == INVALID:   return -1
             
-        #print(values)
+            _ans = max(_ans, max(result))
+            
         return _ans if _ans >= 0 else -1
                     
                     
