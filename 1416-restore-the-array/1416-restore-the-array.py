3b4046313e0de7083c8e548a08111d0a3a703ae2
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-04-23 15:06:17 +0900

    Time: 1655 ms (52.05%), Space: 176.2 MB (12.33%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -8,7 +8,7 @@ class Solution:
             if s[index] == "0": return 0
             value,_return = 0,0
             
-            for i in range(index, min(len(s), index + 11)):
+            for i in range(index, min(len(s), index + 10)):
                 value = value * 10 + int(s[i])
                 if 1 <= value <= k:
                     _return = (_return + dfs(i + 1)) % MOD
