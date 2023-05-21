f9d952ffda580116f8830a035dd7fa22573d5b12
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-05-21 15:52:12 +0900

    Time: 374 ms (86.53%), Space: 16.8 MB (85.24%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -13,19 +13,20 @@ class Solution:
         
         while q:
             x,y = q.popleft()
-            print(x,y)
             for i in range(4):
                 nx, ny = x + dx[i], y + dy[i]                
                 if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and not visit[nx][ny]:
-                    visit[nx][ny] = True
+                    
+                    if grid[nx][ny] and not grid[x][y]:
+                        return visit[x][y] - 1
+                
                     
                     if not grid[nx][ny]:
                         visit[nx][ny] = visit[x][y] + 1
                         q.append((nx,ny))
+                    
+                    
                     else:
-                        if not grid[x][y]:
-                            return visit[x][y] - 1
-                        
                         visit[nx][ny] = visit[x][y]
                         q.appendleft((nx,ny))
         
