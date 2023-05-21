1009a3048abab9e45c2f0dec8a138802735db1c2
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-05-21 16:05:04 +0900

    Time: 359 ms (94.60%), Space: 16.7 MB (86.05%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,37 +1,35 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        _x,_y = 0,0
+        
+        #Get start point of one land by loop
+        startX,startY = 0,0
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]:  _x,_y = i,j
+                if grid[i][j]:  startX,startY = i,j
             
-        
-        
-        visit = [[0] * len(grid[0]) for _ in range(len(grid))]
-        q = deque([(_x,_y)])
-        visit[_x][_y] = 1
+        #Define dp table that dp[x][y] means the nearest distance from start point to node (x,y)
+        distance = [[0] * len(grid[0]) for _ in range(len(grid))]
+        q = deque([(startX,startY)])
+        distance[startX][startY] = 1
         dx,dy = [0,0,1,-1],[1,-1,0,0]
         
         while q:
             x,y = q.popleft()
             for i in range(4):
                 nx, ny = x + dx[i], y + dy[i]                
-                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and not visit[nx][ny]:
-                    
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and not distance[nx][ny]:
+                    #if current node is water, and the next node is land, it'll be the nearest point of another land.
                     if grid[nx][ny] and not grid[x][y]:
-                        return visit[x][y] - 1
-                
+                        return distance[x][y] - 1
                     
                     if not grid[nx][ny]:
-                        visit[nx][ny] = visit[x][y] + 1
+                        distance[nx][ny] = distance[x][y] + 1
                         q.append((nx,ny))
                     
-                    
                     else:
-                        visit[nx][ny] = visit[x][y]
+                        distance[nx][ny] = distance[x][y]
                         q.appendleft((nx,ny))
-        
-        #rint(visit)
             
             
         
