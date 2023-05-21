996965ec21b6cc997696550d4f64a56ec35a6d91
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-05-22 00:37:08 +0900

    Time: 62 ms (5.84%), Space: 16.5 MB (8.81%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        parent = defaultdict(list)
+        
+        def find(node : str) -> list:
+            if node not in parent or parent[node][0] == node:
+                parent[node] = [node,1]
+            
+            else:
+                #print(parent, parent[node][0])
+                parentNode, value = find(parent[node][0])
+                parent[node] = [parentNode, parent[node][1] * value]
+                
+            return parent[node]
+            
+        
+        def union(a : str, b : str, val : int):
+            pa, pb = find(a), find(b)
+            parent[pa[0]] = [pb[0], pb[1] / pa[1] * val]
+        
+        for pair, value in zip(equations, values):
+            union(pair[0], pair[1], value)
+        
+        print([-1 if (a not in parent or b not in parent or find(a)[0] != find(b)[0]) else find(a)[1] / find(b)[1] for a,b in queries])
+        print(parent)
+        return [-1 if (a not in parent or b not in parent or find(a)[0] != find(b)[0]) else find(a)[1] / find(b)[1] for a,b in queries]
+                
+            
+        
+        
