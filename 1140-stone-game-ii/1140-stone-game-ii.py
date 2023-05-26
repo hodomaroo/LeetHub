c503f17045f6551e3d437ff3dda9e510baa9b6fd
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-05-26 18:49:54 +0900

    Time: 301 ms (69.59%), Space: 18.4 MB (34.56%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -4,8 +4,7 @@ class Solution:
         
         #현재 자기가 만들 수 있는 가장큰 값을 반환 -> 근데, 어차피 자기가 제일 커질 수 있는 값을 구하는거임 / 굳이 필요 없다!
         #어차피 플레이어가 누구든, 만들 수 있는 값은 동일!
-        
-        print(prefix)
+    
         @lru_cache(None)
         def dfs(idx : int, m : int) -> int:
 
