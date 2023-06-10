b550e126f8311b4357ac5d53d2dd37a1d1a453e2
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-06-10 18:22:10 +0900

    Time: 46 ms (66.41%), Space: 16.2 MB (53.44%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -5,22 +5,21 @@ class Solution:
         while l + 1 < r:
             mid = (l + r) // 2
             
-            left_sum = (mid) * (mid + 1) // 2
-            right_sum = (mid) * (mid + 1) // 2
+            _sum = (mid) * (mid + 1) - mid
             
             if index >= mid:
-                left_sum += index - mid + 1
+                _sum += index - mid + 1
+            
             if index + 1 < mid:
-                left_sum -= (mid - index - 1) * (mid - index) // 2
+                _sum -= (mid - index - 1) * (mid - index) // 2
             
             if n - index > mid:
-                right_sum += n - index - mid
+                _sum += n - index - mid
             
             if n - index < mid:
-                right_sum -= (mid - (n - index)) * (mid - (n - index) + 1) // 2
-            
-            #print(left_sum, right_sum, mid)
-            if left_sum + right_sum - mid > maxSum:
+                _sum -= (mid - (n - index)) * (mid - (n - index) + 1) // 2
+        
+            if _sum > maxSum:
                 r = mid
             else:
                 l = mid
