c4164aa46717664d7f7e27057f1e656ce9665d50
김두호 <61859590+hodomaroo@users.noreply.github.com>
2023-08-13 22:30:30 +0900

    Time: 958 ms (78.46%), Space: 30.5 MB (68.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -3,14 +3,7 @@ class Solution:
         dp = [False for _ in range(len(nums))] + [True] #[1, 2, 3]
         
         for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
-                dp[i] = max(dp[i], dp[i-2])
-            
-            if i > 1 and nums[i] == nums[i-1] and nums[i-1] == nums[i-2]:
-                dp[i] = max(dp[i], dp[i-3])
-            
-            if i > 1 and nums[i] == nums[i-1] + 1 and nums[i - 2] + 1 == nums[i - 1]:
-                dp[i] = max(dp[i], dp[i-3])
+            dp[i] = max(dp[i], dp[i-2] if nums[i] == nums[i-1] else False,  dp[i-3] if i > 1 and ((nums[i] == nums[i-1] and nums[i-1] == nums[i-2]) or (nums[i-1] + 1 == nums[i] and nums[i-2] + 1 == nums[i-1])) else False)
         
         return dp[-2]
                 
